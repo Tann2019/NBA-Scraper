7cb17d85fd950cee6ff81d7d41632be9593a6e9f
Tanner Steorts <tanner@fencesoftware.com>
2023-01-31 20:52:29 -0700

    add table implementation to read in code easier

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 
 url = 'https://www.basketball-reference.com/teams/MIL/2023.html'
 html_text = requests.get(url).text
-nba = BeautifulSoup(html_text, 'html.parser')
+soup = BeautifulSoup(html_text, 'html.parser')
 
-print(nba.get_text())
+data = []
+table = soup.find('table', attrs={'class':'stats_table'})
+table_head = table.find('thead')
+top_row = table_head.find('tr')
+cols = top_row.find_all('th')
+cols = [ele.text.strip() for ele in cols]
+data.append([ele for ele in cols if ele]) # Get rid of empty values
 
+
+
+table_body = table.find('tbody')
+
+rows = table_body.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele]) # Get rid of empty values
+
+print(tabulate(data, headers='firstrow' ,tablefmt='fancy_grid'))
 
  
