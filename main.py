5687321177b75b332f3e2a8a64427eb5c7b6d9f2
Tanner Steorts <tanner@fencesoftware.com>
2023-01-31 20:33:24 -0700

    start of scraping

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = 'https://www.basketball-reference.com/teams/MIL/2023.html'
+html_text = requests.get(url).text
+nba = BeautifulSoup(html_text, 'html.parser')
+
+print(nba.get_text())
+
+
+ 
